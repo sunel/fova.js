aae2db23a80625450d8480e88027cc9682db27d0
Sunel Tr <sunelbe@gmail.com>
2017-11-27 22:56:11 +0530

    added form pseudo code for easy elements selections

--- src/Selector.js
+++ src/Selector.js
@@ -4,6 +4,23 @@ class Selector {
 
   constructor(expression) {
     this.expression = expression;
+    Selector.addFormPseudos();
+  }
+
+  static addFormPseudos() {
+    sizzle.selectors.pseudos.formEle = (elem) => {
+      if (sizzle.selectors.pseudos.disabled(elem)) {
+        return false;
+      }
+
+      const name = elem.nodeName.toLowerCase();
+      const excludeType = ['button', 'submit', 'reset', 'image'];
+      if ((name === 'input' || name === 'button') && excludeType.includes(elem.type)) {
+        return false;
+      }
+      const rinputs = /^(?:input|select|textarea)$/i;
+      return rinputs.test(elem.nodeName);
+    };
   }
 
   matchs(context = document) {
